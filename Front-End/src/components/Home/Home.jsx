02682406db5c8a4ec8917e35c37d3644d2aaa6c0
Sujitha1221 <sujikanthan2001@gmail.com>
2025-03-17 20:12:01 +0530

    Curated Home Page speifically for our application

--- Front-End/src/components/Home/Home.jsx
+++ Front-End/src/components/Home/Home.jsx
@@ -1,37 +1,27 @@
-import React from 'react';
+import React from "react";
 import "./Home.css";
-import Hero from '../Hero/Hero';
-import TrustedBrand from '../TrustedBrand/TrustedBrand';
-import TopCategories from '../TopCategories/TopCategories';
-import { useLoaderData } from 'react-router-dom';
-import TopCourse from '../TopCourse/TopCourse';
-import PromoSection from '../PromoSection/PromoSection';
+import Hero from "../Hero/Hero";
+import TrustedBrand from "../TrustedBrand/TrustedBrand";
+import TopCategories from "../TopCategories/TopCategories";
+import PromoSection from "../PromoSection/PromoSection";
 
 const Home = () => {
-    // Fetch courses after delay
-    const courses = useLoaderData() || [];
-    const displayCourses = courses.slice(0, 8);
-
-
-    return (
-        <div>
-            <div className='hero-bg-img mt-0 md:mt-14'>
-                <Hero />
-            </div>
-            <div className='my-20'>
-                <TrustedBrand />
-            </div>
-            <div className='my-20'>
-                <TopCategories />
-            </div>
-            <div className='my-20'>
-                <TopCourse displayCourses={displayCourses} />
-            </div>
-            <div className='my-20'>
-                <PromoSection />
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="hero-bg-img mt-0 md:mt-14">
+        <Hero />
+      </div>
+      <div className="my-20">
+        <TrustedBrand />
+      </div>
+      <div className="my-20">
+        <TopCategories />
+      </div>
+      <div className="my-20">
+        <PromoSection />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
