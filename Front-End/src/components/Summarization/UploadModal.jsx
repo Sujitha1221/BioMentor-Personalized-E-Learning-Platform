83afe47e32636e34d2e060c837d9127f26fc741f
IT21068478 <dharanesegar08@gmail.com>
2025-03-18 17:52:11 +0530

    Changed download format

--- Front-End/src/components/Summarization/UploadModal.jsx
+++ Front-End/src/components/Summarization/UploadModal.jsx
@@ -175,7 +175,7 @@ const UploadModal = ({ isOpen, onClose }) => {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", "summary.txt");
+      link.setAttribute("download", "summary.pdf");
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
