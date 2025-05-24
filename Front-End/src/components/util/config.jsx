410aac60c77af4e0dcc94eb0c718003ce7eb8632
IT21068478 <dharanesegar08@gmail.com>
2025-05-24 18:25:04 +0530

    add user url

--- Front-End/src/components/util/config.jsx
+++ Front-End/src/components/util/config.jsx
@@ -6,7 +6,7 @@
 // export const MCQ_URL = "http://127.0.0.1:8003"
 // export const FRONTEND_BASE_URL  = "http://localhost:3000"
 
-export const LOGIN_URL = "http://127.0.0.1:8001";
+export const LOGIN_URL = "http://20.193.152.91/user";
 export const QA_URL = "http://20.193.156.66/QnA";
 export const SUMMARIZE_URL = "http://127.0.0.1";
 export const MCQ_URL = "http://127.0.0.1:8003"
