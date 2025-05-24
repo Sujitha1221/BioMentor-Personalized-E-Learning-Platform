62ff8ded0bb5af9f92db133e25cb3dde7a8092e0
IT21068478 <dharanesegar08@gmail.com>
2025-05-24 19:05:49 +0530

    URL Changes

--- Front-End/src/components/util/config.jsx
+++ Front-End/src/components/util/config.jsx
@@ -8,6 +8,6 @@
 
 export const LOGIN_URL = "http://20.193.152.91/user";
 export const QA_URL = "http://20.193.156.66/QnA";
-export const SUMMARIZE_URL = "http://127.0.0.1";
+export const SUMMARIZE_URL = "http://20.197.16.186";
 export const MCQ_URL = "http://127.0.0.1:8003"
-export const FRONTEND_BASE_URL  = "http://localhost:3000"
+export const FRONTEND_BASE_URL  = "http://20.193.152.91"
