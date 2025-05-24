4d22a54c413fe7e30ca7edc846d0150ca4d2556f
IT21068478 <dharanesegar08@gmail.com>
2025-05-24 17:15:31 +0530

    URL Changes

--- Front-End/src/components/util/config.jsx
+++ Front-End/src/components/util/config.jsx
@@ -1,7 +1,13 @@
 // src/config/common.js
 
+// export const LOGIN_URL = "http://127.0.0.1:8001";
+// export const QA_URL = "http://127.0.0.1:8000";
+// export const SUMMARIZE_URL = "http://127.0.0.1:8002";
+// export const MCQ_URL = "http://127.0.0.1:8003"
+// export const FRONTEND_BASE_URL  = "http://localhost:3000"
+
 export const LOGIN_URL = "http://127.0.0.1:8001";
-export const QA_URL = "http://127.0.0.1:8000";
-export const SUMMARIZE_URL = "http://127.0.0.1:8002";
+export const QA_URL = "http://20.193.156.66/QnA";
+export const SUMMARIZE_URL = "http://127.0.0.1";
 export const MCQ_URL = "http://127.0.0.1:8003"
 export const FRONTEND_BASE_URL  = "http://localhost:3000"
